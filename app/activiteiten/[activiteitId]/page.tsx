493b5a88cb562615c6107aa6197fc68d5ff89e8c
Alex <lamp0020@hz.nl>
2024-11-19 21:35:15 +0100

    Activiteit Specific Page Update

--- app/activiteiten/[activiteitId]/page.tsx
+++ app/activiteiten/[activiteitId]/page.tsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 const ActiviteitPage = () => {
   return (
-    <div>
-      <h1>Activiteit Page</h1>
+    <div className='max-w-full mx-auto min-h-screen lg:py-20 lg:px-20 px-6 py-6'>
+      <h1 className='text-black text-2xl mb-8 font-semibold'>Activiteit Pagina</h1>
+      <p className='italic text-gray-500 text-sm'>De inhoud van deze pagina moet nog worden toegevoegd.</p>
     </div>
   )
 }
